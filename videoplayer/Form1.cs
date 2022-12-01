14951ef24709c693268c09a23939cf90deacba67
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-02 00:25:35 +0200

    Add project files.

---
+++ videoplayer/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Windows.Media;
+using System.Windows;
+using System.IO;
+using System.Runtime;
+using System.Diagnostics;
+
+
+namespace videoplayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Windows.Media.MediaPlayer p = new System.Windows.Media.MediaPlayer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string s = "sample.mp4";
+            Uri u = new Uri(Application.StartupPath + "\\" + s);
+            Text = u.ToString();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            //"C:\\ProgramFiles(x86)\\Windows Media Player\\";
+            startInfo.FileName = "wmplayer.exe";
+            startInfo.Arguments = Application.StartupPath + "\\" + s;
+            Process.Start(startInfo);
+            p.Open(u);
+            
+            p.Play();
+        }
+    }
+}
